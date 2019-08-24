ff4441214e62db943a0b873cfc05b75edd66f249
root <root@instance-np8pryk9.(none)>
2019-08-24 17:32:33 +0800

    使用字符串

---
+++ plus_chapter_4/4.2_strings/4.2.2_use_string/01_praisel.c
@@ -0,0 +1,14 @@
+/* praisel.c -- 使用不同类型的字符串*/
+# include <stdio.h>
+# define PRAISE "You are an extraodinary being"
+
+int main(void)
+{
+	char name[40];
+	
+	printf("What's your name?\n");
+	scanf("%s", name);
+	printf("Hello, %s. %s\n", name, PRAISE);
+	
+	return 0;
+}

58f351361c2134c1d22843a2eaa8aac084956dce
root <1962610298@qq.com>
2019-09-07 15:58:35 +0800

    带参数的函数

---
+++ plus_chapter_5/5.6_function_with_parameters/pound.c
@@ -0,0 +1,22 @@
+/* pound.c -- 定义一个带一个参数的函数 */
+# include <stdio.h>
+
+int main(void)
+{
+	int times = 5;
+	char ch = '!';
+	float f = 6.0;
+
+	pound(times);
+	pound(ch);
+	pound(f);
+
+	return 0;
+}
+
+void pound(int n)
+{
+	while (n-- > 0)
+		printf("#");
+	printf("\n");
+}

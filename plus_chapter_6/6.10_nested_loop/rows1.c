7c898e9375854d8e194607a28d5b07653aac8ef1
root <1962610298@qq.com>
2019-09-14 19:27:59 +0800

    嵌套循环

---
+++ plus_chapter_6/6.10_nested_loop/rows1.c
@@ -0,0 +1,21 @@
+/* rows1.c -- 使用嵌套循环 */
+# include <stdio.h>
+
+int main(void)
+{
+	const int ROWS = 6;
+	const int CHARS = 10;
+	int row;
+	char ch;
+
+	for (row = 0; row < ROWS; row++)
+	{
+		for (ch = 'A'; ch < ('A' + CHARS); ch++)
+		{
+			printf("%c", ch);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}

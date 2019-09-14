26ad740020f6d131877132a145ad2c90d4d1aca0
root <1962610298@qq.com>
2019-09-14 15:36:06 +0800

    不确定循环和计数循环

---
+++ plus_chapter_6/6.4_uncorrect_loop_count_loop/sweetie1.c
@@ -0,0 +1,17 @@
+/* sweetie1.c -- 一个技术循环 */
+# include <stdio.h>
+
+int main(void)
+{
+	const int NUMBER = 22;
+	int count = 1;
+
+	while (count <= 22)
+	{
+		printf("Be my Valentine!");
+		count++;
+		printf("%d", count);
+	}
+
+	return 0;
+}

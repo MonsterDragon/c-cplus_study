a32cb9e74a57c3db27bbb4db80c50bbb0d11e59b
root <1962610298@qq.com>
2019-09-14 20:59:30 +0800

    使用函数返回值的循环示例

---
+++ plus_chapter_6/6.12_use_return_from_function/power.c
@@ -0,0 +1,33 @@
+/* power.c -- 计算数的整数幂 */
+# include <stdio.h>
+
+double power(double n, int p); // ANSI函数原型
+int main(void)
+{
+	double x, xpow;
+	int exp;
+
+	printf("Enter a number and positive integer power");
+	printf(" to which\nthe number will be raised. Enter q");
+	printf(" to quit.\n");
+	while (scanf("%lf%d", &x, &exp) == 2)
+	{
+		xpow = power(x, exp); // 函数调用
+		printf("%.3g to the power %d is %.5g\n", x, exp, xpow);
+		printf("Enter next pair of numbers or q to quit.\n");
+	}
+	printf("Hope you enjoyed this power trip!\n");
+}
+
+double power(double n, int p)
+{
+	double pow = 1;
+	int i;
+
+	for (i = 0; i < p; i++)
+	{
+		pow *= n;		
+	}
+
+	return pow; // 返回pow值
+}

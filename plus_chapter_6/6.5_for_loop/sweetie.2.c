9c300ed6d061b0c57bb9143c2183553ea71f2fe9
root <1962610298@qq.com>
2019-09-14 15:54:49 +0800

    for 循环

---
+++ plus_chapter_6/6.5_for_loop/sweetie.2.c
@@ -0,0 +1,15 @@
+/* sweetie.c -- 使用for循环的计数循环 */
+# include <stdio.h>
+
+int main(void)
+{
+	const int NUMBER = 22;
+	int count;
+
+	for (count = 1; count <= NUMBER; count++)
+	{
+		printf("Be my valentine!\n");
+	}
+
+	return 0;
+}

19fa05ed9c2fdee4582cfda9f4908b5bf2fc041c
root <1962610298@qq.com>
2019-09-21 16:54:38 +0800

    ctype.h系列的字符函数

---
+++ plus_chapter_7_branch_jump/7.2_if_else_statement/7.2.2_ctype.h_char_function/cypher2.c
@@ -0,0 +1,23 @@
+/* cypher2.c -- 替换输入的字符，非字母字符保持不变 */
+# include <stdio.h>
+# include <ctype.h>
+
+int main(void)
+{
+	char ch;
+
+	while ((ch = getchar()) != '\n')
+	{
+		if (isalpha(ch))
+		{
+			putchar(ch + 1);
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar(ch);
+
+	return 0;
+}

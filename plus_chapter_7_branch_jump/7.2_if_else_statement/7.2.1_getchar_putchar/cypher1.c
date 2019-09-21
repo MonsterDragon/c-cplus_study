839050af6f3e2cda45adebfa060ec59cbb312ded
root <1962610298@qq.com>
2019-09-21 16:37:44 +0800

    getchar()和putchar()

---
+++ plus_chapter_7_branch_jump/7.2_if_else_statement/7.2.1_getchar_putchar/cypher1.c
@@ -0,0 +1,25 @@
+/* cyper1.c -- 更改输入，空格不变 */
+# include <stdio.h>
+# define SPACE ' ' // SPACE表示单引号-空格-单引号
+
+int main(void)
+{
+	char ch;
+	
+	ch = getchar();
+	while (ch != '\n')
+	{
+		if (ch == SPACE)
+		{
+			putchar(ch);
+		}
+		else
+		{
+			putchar(ch+1);
+		}
+		ch = getchar();
+	}
+	putchar(ch);
+
+	return 0;
+}

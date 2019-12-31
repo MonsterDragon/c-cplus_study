43c7a6a5ec16a3739ac657d96c464655819dc957
root <1962610298@qq.com>
2019-12-31 22:10:11 +0800

    初始化数组

---
+++ chapter_10_array_pointer/10.1_array/10.1.1_init_array/day_mon1.c
@@ -0,0 +1,16 @@
+/* day_mon1.c -- 打印每个月的天数 */
+#include <stdio.h>
+#define MONTHS 12
+
+int main(void)
+{
+	int days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int index;
+
+	for (index = 0; index < 12; index++)
+	{
+		printf("Month %2d has %2d days.\n", index + 1, days[index]);
+	}
+
+	return;
+}

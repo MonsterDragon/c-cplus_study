13681a83328c2660ed6d7be7617d4a24dbab69b9
root <1962610298@qq.com>
2020-01-09 21:58:49 +0800

    update 9_p files

--- chapter_10_array_pointer/10.13_coding_prctice/9_p.c
+++ chapter_10_array_pointer/10.13_coding_prctice/9_p.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int ** arr_1(int rows, int cols, int arr[rows][cols]);
-void arr_2(int rows, int cols, int arr[rows][cols]);
+void arr_2(int rows, int cols, int ** arr);
 
 int main()
 {
@@ -14,13 +14,22 @@ int main()
 	};
 	// int (*pr)[5];
 	int ** pr;
+	int (*ar)[5];
 	int rows, cols;
 
 	cols = sizeof(*arr) / sizeof(int);
 	rows = (sizeof(arr) / sizeof(int)) / (sizeof(*arr) / sizeof(int));
 	printf("The size of arr is %d\n", rows);
 
+	pr = (int **)malloc(rows * sizeof(int *));
+	for (int i = 0; i < rows; i++)
+	{
+		pr[i] = (int *)malloc(cols * sizeof(int));
+	}
 	pr = arr_1(rows, cols, arr);
+	ar = pr;
+	arr_2(rows, cols, arr);
+	arr_2(rows, cols, ar);
 }
 
 int ** arr_1(int rows, int cols, int arr[rows][cols])
@@ -42,4 +51,18 @@ int ** arr_1(int rows, int cols, int arr[rows][cols])
 	}
 
 	return pr;
+}
+
+void arr_2(int rows, int cols, int ** arr)
+{
+	int m, n;
+
+	for (m = 0; m < rows; m++)
+	{
+		for (n = 0; n < cols; n++)
+		{
+			printf("%d ", arr[m][n]);
+		}
+		printf("\n");
+	}
 }
